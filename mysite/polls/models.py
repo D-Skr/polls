39a99b205f2f1a12b14052d2ea00d781eb77b5d6
Dmitriy <altusj@gmail.com>
2024-01-09 18:38:18 -0500

    polls Django app

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.datetime_safe import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        from django.utils import timezone
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class  Choice(models.Model):
